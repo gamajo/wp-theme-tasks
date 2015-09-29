a3a419e6eadfd745725cfe422c132b34e2d9975f
Travis Smith <t@wpsmith.net>
2015-09-29 11:37:26 -0400

    Add grunt shell task

---
+++ config/grunt/shell.js
@@ -0,0 +1,8 @@
+// https://github.com/sindresorhus/grunt-shell
+/*global module*/
+module.exports = {
+	grunt: {
+		//command: 'afplay grunt.mp3' // Mac
+		command: 'powershell -c echo `a' // Windows
+	}
+};
